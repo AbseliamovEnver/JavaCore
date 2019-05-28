1c6bb112c987a86cfa06f120710ac0b5b082b8e4
AbseliamovEnver <a.enver.com@gmail.com>
2019-05-28 18:38:08 +0300

    Second commit - LinkedList.

---
+++ src/main/java/com/abselyamov/javacore/chapter18/LinkedListDemo.java
@@ -0,0 +1,47 @@
+package com.abselyamov.javacore.chapter18;
+
+import java.util.LinkedList;
+
+/**
+ * @author Enver on 28.05.2019 18:16.
+ * @project javacore
+ */
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        // Create a linked list.
+        LinkedList<String> ll = new LinkedList<String>();
+
+        // Add elements to the linked list.
+        ll.add("F");
+        ll.add("B");
+        ll.add("D");
+        ll.add("E");
+        ll.add("C");
+
+        ll.addLast("Z");
+        ll.addFirst("A");
+
+        ll.add(1, "A2");
+
+        System.out.println("Original contents of ll: " + ll);
+
+        // Remove elements from the linked list.
+        ll.remove("F");
+        ll.remove(2);
+
+        System.out.println("Contents of ll after deletion: " + ll);
+
+        // Remove first and last elements.
+        ll.removeFirst();
+        ll.removeLast();
+
+        System.out.println("Contents of ll after deleting first and last: " + ll);
+
+        // Get and set a value.
+
+        String value = ll.get(2);
+        ll.set(2, value + " changed");
+
+        System.out.println("Contents of ll after change: " + ll);
+    }
+}
